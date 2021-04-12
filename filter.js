57bff8d582a986da06adc9d86173156f5c68aaf1
Konrad Faltyn <faltyn.konrad@gmail.com>
2021-04-12 13:37:11 +0200

    fixes

--- filter.js
+++ filter.js
@@ -1,10 +1,10 @@
 var stream = require('stream')
+var fixes = require('./fixes')
 
 module.exports = {
     buffer: null,
 
     bindSockets: function(socket, proxySocket) {
-        
         const transform = this.transformStream()
         proxySocket.pipe(transform).pipe(socket).pipe(proxySocket)
     },
@@ -15,7 +15,6 @@ module.exports = {
         const _filter = this
         transform._transform = function (chunk, encoding, done) {
             _filter.gather(chunk, encoding, this)
-
             done()
         }
         return transform
@@ -36,23 +35,57 @@ module.exports = {
     },
 
     edit: function(transform) {
-        const originalMessage = String(Buffer.from(this.buffer, 4, 0))
-        const prefix = Buffer.from(this.buffer, 0, 4)
-        const jsonString = originalMessage.slice(4)
+        const header = new DataView(this.buffer.buffer)
+        const l1 = header.getUint8(1)
+        const l2 = header.getUint16(2, false)
+        const l3 = header.getBigUint64(4, false)
+
+        let headerSize 
+        let payloadSize
+        if(l1 < 126) {
+            payloadSize = l1
+            headerSize = 2
+        }else if(l1 == 126) {
+            payloadSize = l2
+            headerSize = 4
+        }else if(l1 == 127) {
+            headerSize = 8
+            payloadSize = l3
+        }
+
+        const originalMessage = String(this.buffer)
+        const jsonString = originalMessage.slice(headerSize)
         var json = JSON.parse(jsonString)
 
+        const originalLen = jsonString.length
         json = this.fix(json)
         
-        const lateJsonString = JSON.stringify(json)
-        const edited = Buffer.concat([prefix, Buffer.from(lateJsonString)])
-        const result = Buffer.compare(this.buffer, edited)
+        const modJsonString = JSON.stringify(json)
+        const modJsonStringLen = modJsonString.length
 
-        transform.push(this.buffer)
+        const modHeader = Buffer.alloc(headerSize, 0)
+        this.buffer.copy(modHeader, 0, 0, headerSize)
+
+        if(modJsonStringLen != originalLen) {
+            const header2 = new DataView(modHeader.buffer)
+            if(l1 < 126) {
+                header2.setUint8(1, modJsonStringLen)
+            }else if(l1 == 126) {
+                header2.setUint16(2, modJsonStringLen)
+            }else if(l1 == 127) {
+                header2.setUint64(4, modJsonStringLen)
+            }
+        }
+
+
+        const finalModBuff = Buffer.concat([modHeader, Buffer.from(modJsonString)])
+        transform.push(finalModBuff)
         this.buffer = null
     },
 
     fix: function(json) {
-        console.dir(json)
+        const response = json.result
+        fixes.do(response)
         return json
     }
 }
---
+++ fixes.js
@@ -0,0 +1,34 @@
+function prefixCheck(obj) {
+    if(!obj || obj.length < 4) {
+        return false
+    }
+    if(obj.indexOf("0x0") == -1) {
+        return false
+    }
+    return true
+}
+
+const requiredKeys = ['v', 'r', 's']
+const shortEncodingKeys = ["nonce", "gasPrice", "gasLimit", "value", "input", "v", "r", "s"]
+
+module.exports = {
+    do: function(res) {
+        this.leadingZeroes(res)
+    },
+
+    leadingZeroes: function(json) {
+
+        json.transactions.forEach(t => {
+            for(const key in t) {
+                if(requiredKeys.includes(key) && t[key] == null) {
+                    t[key] = '0x00'
+                }
+                if(shortEncodingKeys.includes(key) && prefixCheck(t[key])) {
+                    t[key] = t[key].replace("0x0", "0x")
+                }
+            }
+        })
+
+        console.log('a')
+    },
+}
