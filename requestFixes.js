32d652f54c805273015719d942a1a9c2a3853a64
Konrad Faltyn <faltyn.konrad@gmail.com>
2021-04-14 17:35:13 +0200

    Cleanup

--- requestFixes.js
+++ requestFixes.js
@@ -8,11 +8,9 @@ module.exports = {
     },
 
     fixNulls: function(json) {
-        console.log('a')
         if(!json.params || !json.params[1] || !json.params[1].topics) {
             return
         }
         json.params[1].topics = json.params[1].topics.filter(el => el != null)
-        console.log('a')
     }
 }
